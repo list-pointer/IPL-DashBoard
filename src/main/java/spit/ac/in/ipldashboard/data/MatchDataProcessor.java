44ee3f3d438dd4101eb6b090806bf143aa39bf9f
Abhishek Gupta <guptaabhishek891@gmail.com>
2021-07-10 07:19:04 +0530

    created classes for spring batch processing and mapped the input data to output data

---
+++ src/main/java/spit/ac/in/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,44 @@
+package spit.ac.in.ipldashboard.data;
+
+import org.springframework.batch.item.ItemProcessor;
+import spit.ac.in.ipldashboard.model.Match;
+
+import java.time.LocalDate;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+
+    @Override
+    public Match process(MatchInput matchInput) throws Exception {
+
+        String firstInningsTeam, secondInningsTeam;
+
+        Match match = new Match();
+
+        match.setId(Long.parseLong(matchInput.getId()));
+        match.setCity(matchInput.getCity());
+        match.setDate(LocalDate.parse(matchInput.getDate()));
+        match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+        match.setVenue(matchInput.getVenue());
+
+        if ("bat".equals(matchInput.getToss_decision())) {
+            firstInningsTeam = matchInput.getToss_winner();
+            secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) ? matchInput.getTeam2() : matchInput.getTeam1();
+        } else {
+            firstInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) ? matchInput.getTeam2() : matchInput.getTeam1();
+            secondInningsTeam = matchInput.getToss_winner();
+        }
+
+        match.setTeam1(matchInput.getTeam1());
+        match.setTeam2(matchInput.getTeam2());
+        match.setTossWinner(matchInput.getToss_winner());
+        match.setTossDecision(matchInput.getToss_decision());
+        match.setMatchWinner(matchInput.getWinner());
+        match.setResult(matchInput.getResult());
+        match.setResultMargin(matchInput.getResult_margin());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(matchInput.getUmpire2());
+
+        return match;
+    }
+}
